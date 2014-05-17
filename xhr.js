7202dbc73286d8e92b648080c87f6008fdf07eb3
gregor <info@gregorkofler.com>
2014-05-17 23:14:46 +0200

    Removed JSON.js code from file, since this was only necessary for IE6/7; added preliminary support for file upload requests; added vxJS.xhr.setHeader(field, value) method
    Change-Id: I17e385bf20578bad2d858ec6cc4ae43d0f11ce66

--- xhr.js
+++ xhr.js
@@ -8,7 +8,7 @@
  *
  * @returns xhr object
  *
- * @version 3.8.4 2013-09-26
+ * @version 4.0.0 2014-05-17
  * @author Gregor Kofler
  *
  * served events: "timeout", "complete", "fail", "beforeSend"
@@ -40,7 +40,9 @@ vxJS.xhr = function(req, param, anim, cb) {
 	if(!param)	{ param = {}; }
 	if(!anim)	{ anim = {}; }
 
-	var	timeout = req.timeout || 5000, timer, xhrO = vxJS.xhrObj(), that = { response: {} };
+	var	timeout = req.timeout || 5000, timer,
+		headers = {},
+		xhrO = vxJS.xhrObj(), that = { response: {} };
 
 	var stopTimer = function() {
 		if(timer) {
@@ -105,8 +107,12 @@ vxJS.xhr = function(req, param, anim, cb) {
 		}
 	};
 
+	var setHeader = function(field, value) {
+		headers[field] = value;
+	};
+
 	var submit = function() {
-		var l = window.location,
+		var l = window.location, i, f,
 			uri = encodeURI(req.uri || (l.hash ? l.href.substring(0, l.href.indexOf(l.hash)) : l.href));
 
 		abort();
@@ -114,32 +120,54 @@ vxJS.xhr = function(req, param, anim, cb) {
 		param.httpRequest = req.command || "";
 		param.echo = req.echo ? 1 : 0;
 
+		setHeader("X-Requested-With", "XMLHttpRequest");
+
 		if(req.forceXMLResponse && xhrO.overrideMimeType) {
 			xhrO.overrideMimeType("text/xml");
 		}
 
 		if(req.method && req.method.toUpperCase() == "GET") {
 			xhrO.open( "GET", uri += (uri.indexOf("?") !== -1 ? "&" : "?") + (new Date()).getTime() + "&xmlHttpRequest=" + encodeURIComponent(JSON.stringify(param)), true);
-			xhrO.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+			for(i = 0, f = Object.keys(headers); i < f.length; ++i) {
+				xhrO.setRequestHeader(f[i], headers[f[i]]);
+			}
 			xhrO.onreadystatechange = stateChange;
 			vxJS.event.serve(that, "beforeSend");
 			xhrO.send(null);
 		}
+
 		else {
 			xhrO.open("POST",uri, true);
-			xhrO.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-			xhrO.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-			xhrO.onreadystatechange = stateChange;
-			vxJS.event.serve(that, "beforeSend");
-			xhrO.send("xmlHttpRequest="+encodeURIComponent(JSON.stringify(param)));
+
+			if(!req.upload) {
+				setHeader("Content-Type", "application/x-www-form-urlencoded");
+				for(i = 0, f = Object.keys(headers); i < f.length; ++i) {
+					xhrO.setRequestHeader(f[i], headers[f[i]]);
+				}
+				xhrO.onreadystatechange = stateChange;
+				vxJS.event.serve(that, "beforeSend");
+				xhrO.send("xmlHttpRequest="+encodeURIComponent(JSON.stringify(param)));
+			}
+
+			else if(param.file) {
+				setHeader("X-Filename", param.filename || param.file.name);
+				for(i = 0, f = Object.keys(headers); i < f.length; ++i) {
+					xhrO.setRequestHeader(f[i], headers[f[i]]);
+				}
+				xhrO.onreadystatechange = stateChange;
+				vxJS.event.serve(that, "beforeSend");
+				xhrO.send(param.file);
+			}
 		}
 		startTimer();
 	};
 
-	that.xhrObj	= xhrO;
-	that.abort	= abort;
-	that.submit = submit;
-	that.use	= function(r, p, a, c) {
+	that.setHeader = function(field, value) {
+		setHeader(field, value);
+		return this;
+	};
+
+	that.use = function(r, p, a, c) {
 		vxJS.merge(req,		r);
 		vxJS.merge(param,	p);
 		vxJS.merge(anim,	a);
@@ -152,37 +180,9 @@ vxJS.xhr = function(req, param, anim, cb) {
 		return this;
 	};
 
+	that.xhrObj	= xhrO;
+	that.abort	= abort;
+	that.submit = submit;
+
 	return that;
 };
-
-/**
- * JSON encoding/decoding by Douglas Crockford https://github.com/douglascrockford/JSON-js
- */
-if(!this.JSON){this.JSON={};}
-(function(){"use strict";function f(n){return n<10?'0'+n:n;}
-if(typeof Date.prototype.toJSON!=='function'){Date.prototype.toJSON=function(key){return isFinite(this.valueOf())?this.getUTCFullYear()+'-'+
-f(this.getUTCMonth()+1)+'-'+
-f(this.getUTCDate())+'T'+
-f(this.getUTCHours())+':'+
-f(this.getUTCMinutes())+':'+
-f(this.getUTCSeconds())+'Z':null;};String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(key){return this.valueOf();};}
-var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;function quote(string){escapable.lastIndex=0;return escapable.test(string)?'"'+string.replace(escapable,function(a){var c=meta[a];return typeof c==='string'?c:'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4);})+'"':'"'+string+'"';}
-function str(key,holder){var i,k,v,length,mind=gap,partial,value=holder[key];if(value&&typeof value==='object'&&typeof value.toJSON==='function'){value=value.toJSON(key);}
-if(typeof rep==='function'){value=rep.call(holder,key,value);}
-switch(typeof value){case'string':return quote(value);case'number':return isFinite(value)?String(value):'null';case'boolean':case'null':return String(value);case'object':if(!value){return'null';}
-gap+=indent;partial=[];if(Object.prototype.toString.apply(value)==='[object Array]'){length=value.length;for(i=0;i<length;i+=1){partial[i]=str(i,value)||'null';}
-v=partial.length===0?'[]':gap?'[\n'+gap+
-partial.join(',\n'+gap)+'\n'+
-mind+']':'['+partial.join(',')+']';gap=mind;return v;}
-if(rep&&typeof rep==='object'){length=rep.length;for(i=0;i<length;i+=1){k=rep[i];if(typeof k==='string'){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}else{for(k in value){if(Object.hasOwnProperty.call(value,k)){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}
-v=partial.length===0?'{}':gap?'{\n'+gap+partial.join(',\n'+gap)+'\n'+
-mind+'}':'{'+partial.join(',')+'}';gap=mind;return v;}}
-if(typeof JSON.stringify!=='function'){JSON.stringify=function(value,replacer,space){var i;gap='';indent='';if(typeof space==='number'){for(i=0;i<space;i+=1){indent+=' ';}}else if(typeof space==='string'){indent=space;}
-rep=replacer;if(replacer&&typeof replacer!=='function'&&(typeof replacer!=='object'||typeof replacer.length!=='number')){throw new Error('JSON.stringify');}
-return str('',{'':value});};}
-if(typeof JSON.parse!=='function'){JSON.parse=function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v;}else{delete value[k];}}}}
-return reviver.call(holder,key,value);}
-text=String(text);cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return'\\u'+
-('0000'+a.charCodeAt(0).toString(16)).slice(-4);});}
-if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j;}
-throw new SyntaxError('JSON.parse');};}}());
